2401d6774a60452f7f732ae93a6305e25ac7de1b
daryagd <grishinadaryadm@gmail.com>
2021-11-22 19:10:36 +0300

    add postgres

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,33 +1,33 @@
-INSERT INTO product VALUES ( 123, 'controller' );
-INSERT INTO product VALUES ( 55, 'microchip' );
-INSERT INTO product VALUES ( 34, 'processor' );
-INSERT INTO product VALUES ( 71, 'machine1' );
-INSERT INTO product VALUES ( 72, 'machine2' );
+INSERT INTO product (product_id, product_name) VALUES ( 123, 'controller' );
+INSERT INTO product (product_id, product_name) VALUES ( 55, 'microchip' );
+INSERT INTO product (product_id, product_name) VALUES ( 34, 'processor' );
+INSERT INTO product (product_id, product_name) VALUES ( 71, 'machine1' );
+INSERT INTO product (product_id, product_name) VALUES ( 72, 'machine2' );
 
-INSERT INTO product_composition VALUES (71, 34, 1);
-INSERT INTO product_composition VALUES (71, 55, 5);
-INSERT INTO product_composition VALUES (71, 123, 13);
-INSERT INTO product_composition VALUES (72, 34, 3);
-INSERT INTO product_composition VALUES (72, 123, 6);
+INSERT INTO product_composition (parent_id, child_id, amount) VALUES (71, 34, 1);
+INSERT INTO product_composition (parent_id, child_id, amount) VALUES (71, 55, 5);
+INSERT INTO product_composition (parent_id, child_id, amount) VALUES (71, 123, 13);
+INSERT INTO product_composition (parent_id, child_id, amount) VALUES (72, 34, 3);
+INSERT INTO product_composition (parent_id, child_id, amount) VALUES (72, 123, 6);
 
-INSERT INTO store VALUES ( 12, 'store1', 'moskvoreche' );
-INSERT INTO store VALUES ( 13, 'store2', 'kashira' );
-INSERT INTO store VALUES ( 14, 'store3', 'proletarskiy' );
-INSERT INTO store VALUES ( 15, 'store4', 'leninskiy' );
+INSERT INTO store (store_id, store_name, address) VALUES ( 12, 'store1', 'moskvoreche' );
+INSERT INTO store (store_id, store_name, address) VALUES ( 13, 'store2', 'kashira' );
+INSERT INTO store (store_id, store_name, address) VALUES ( 14, 'store3', 'proletarskiy' );
+INSERT INTO store (store_id, store_name, address) VALUES ( 15, 'store4', 'leninskiy' );
 
-INSERT INTO good VALUES (1, 12, 56, 'microchips', 55);
-INSERT INTO good VALUES (2, 12, 100, 'controllers', 123);
-INSERT INTO good VALUES (3, 13, 2, 'machine1', 71);
-INSERT INTO good VALUES (4, 15, 4, 'machine2', 72);
+INSERT INTO good (good_id, store_id, amount, note, product_id ) VALUES (5, 12, 56, 'microchips', 55);
+INSERT INTO good (good_id, store_id, amount, note, product_id ) VALUES (2, 12, 100, 'controllers', 123);
+INSERT INTO good (good_id, store_id, amount, note, product_id ) VALUES (3, 13, 2, 'machine1', 71);
+INSERT INTO good (good_id, store_id, amount, note, product_id ) VALUES (4, 15, 4, 'machine2', 72);
 
-INSERT INTO employee VALUES (1, 'Kolya', 'Ivanov', 'Petrovich', 'Software');
-INSERT INTO employee VALUES (3, 'Irina', 'Ivanova', 'Petrovna', 'Documents');
-INSERT INTO employee VALUES (6, 'Ura', 'Nikolaev', 'Alexeevich', 'Testing');
-INSERT INTO employee VALUES (9, 'Olga', 'Opasnova', 'Egorovna', 'Development');
+INSERT INTO employee (employee_id, first_name, last_name, middle_name, department) VALUES (1, 'Kolya', 'Ivanov', 'Petrovich', 'Software');
+INSERT INTO employee (employee_id, first_name, last_name, middle_name, department) VALUES (3, 'Irina', 'Ivanova', 'Petrovna', 'Documents');
+INSERT INTO employee (employee_id, first_name, last_name, middle_name, department) VALUES (6, 'Ura', 'Nikolaev', 'Alexeevich', 'Testing');
+INSERT INTO employee (employee_id, first_name, last_name, middle_name, department) VALUES (9, 'Olga', 'Opasnova', 'Egorovna', 'Development');
 
-INSERT INTO employee_store VALUES (1, 12, '2021-10-04');
-INSERT INTO employee_store VALUES (1, 13, '2021-10-04');
-INSERT INTO employee_store VALUES (1, 14, '2021-10-04');
-INSERT INTO employee_store VALUES (3, 12, '2021-12-05');
-INSERT INTO employee_store VALUES (3, 13, '2021-12-04');
-INSERT INTO employee_store VALUES (9, 12, '2021-11-07');
+INSERT INTO employee_store (employee_id, store_id, expiration_date) VALUES (1, 12, '2021-10-04');
+INSERT INTO employee_store (employee_id, store_id, expiration_date) VALUES (1, 13, '2021-10-04');
+INSERT INTO employee_store (employee_id, store_id, expiration_date) VALUES (1, 14, '2021-10-04');
+INSERT INTO employee_store (employee_id, store_id, expiration_date) VALUES (3, 12, '2021-12-05');
+INSERT INTO employee_store (employee_id, store_id, expiration_date) VALUES (3, 13, '2021-12-04');
+INSERT INTO employee_store (employee_id, store_id, expiration_date) VALUES (9, 12, '2021-11-07');
--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,43 +1,43 @@
-CREATE TABLE store (
-    store_id LONG,
-    store_name VARCHAR(20),
-    address VARCHAR(50),
-    PRIMARY KEY (store_id)
-);
-CREATE TABLE good (
-    good_id LONG,
-    store_id LONG,
-    amount LONG,
-    note VARCHAR(50),
-    product_id LONG,
-    PRIMARY KEY (good_id, store_id),
-    FOREIGN KEY (store_id) REFERENCES store(store_id)
-);
-CREATE TABLE product (
-    product_id LONG,
-    product_name VARCHAR(30)
-);
-CREATE TABLE product_composition (
-    parent_id LONG,
-    child_id LONG,
-    amount LONG,
-    PRIMARY KEY (parent_id, child_id),
-    FOREIGN KEY (parent_id) REFERENCES product(product_id),
-    FOREIGN KEY (child_id) REFERENCES product(product_id)
-);
-CREATE TABLE employee (
-    employee_id LONG,
-    first_name VARCHAR(20),
-    last_name VARCHAR(20),
-    middle_name VARCHAR(20),
-    department VARCHAR(30),
-    PRIMARY KEY (employee_id)
-);
-CREATE TABLE employee_store (
-    employee_id LONG,
-    store_id LONG,
-    expiration_date DATE,
-    PRIMARY KEY (employee_id, store_id),
-    FOREIGN KEY (employee_id) REFERENCES employee(employee_id),
-    FOREIGN KEY (store_id) REFERENCES store(store_id)
-);
+-- CREATE TABLE store (
+--     store_id LONG,
+--     store_name VARCHAR(20),
+--     address VARCHAR(50),
+--     PRIMARY KEY (store_id)
+-- );
+-- CREATE TABLE good (
+--     good_id LONG,
+--     store_id LONG,
+--     amount LONG,
+--     note VARCHAR(50),
+--     product_id LONG,
+--     PRIMARY KEY (good_id, store_id),
+--     FOREIGN KEY (store_id) REFERENCES store(store_id)
+-- );
+-- CREATE TABLE product (
+--     product_id LONG,
+--     product_name VARCHAR(30)
+-- );
+-- CREATE TABLE product_composition (
+--     parent_id LONG,
+--     child_id LONG,
+--     amount LONG,
+--     PRIMARY KEY (parent_id, child_id),
+--     FOREIGN KEY (parent_id) REFERENCES product(product_id),
+--     FOREIGN KEY (child_id) REFERENCES product(product_id)
+-- );
+-- CREATE TABLE employee (
+--     employee_id LONG,
+--     first_name VARCHAR(20),
+--     last_name VARCHAR(20),
+--     middle_name VARCHAR(20),
+--     department VARCHAR(30),
+--     PRIMARY KEY (employee_id)
+-- );
+-- CREATE TABLE employee_store (
+--     employee_id LONG,
+--     store_id LONG,
+--     expiration_date DATE,
+--     PRIMARY KEY (employee_id, store_id),
+--     FOREIGN KEY (employee_id) REFERENCES employee(employee_id),
+--     FOREIGN KEY (store_id) REFERENCES store(store_id)
+-- );
